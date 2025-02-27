dc712197f5dfd639ec865eb581ba0fc4575aa81b
seriojaa <119433399+sergiojaa@users.noreply.github.com>
2025-02-27 17:32:09 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// 1st task
+console.log(`ba${2 / "na"}a`)
+
+// 2nd task
+const userNumber = Number(prompt('Enter your number'))
+
+function numToBinary(num) {
+    return num.toString(2)
+}
+
+console.log(numToBinary(userNumber))
+
+
+
+
